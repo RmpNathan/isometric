a2b771e9f4243e823bddf2d067804630699fe3d1
NathanRampillon <nathan@amplitudes.com>
2023-06-21 00:01:47 +0200

    try 3D with three.js

---
+++ src/components/Traviso.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+import * as TRAVISO from 'traviso.js';
+export default {
+  name: 'Traviso',
+  data: () => ({
+    pixiRoot: new PIXI.Application({
+      width: 800,
+      height: 600,
+      backgroundColor: 0xffffff,
+    })
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
